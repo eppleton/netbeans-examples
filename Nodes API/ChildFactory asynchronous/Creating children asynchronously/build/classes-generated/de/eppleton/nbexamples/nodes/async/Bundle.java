9e8eb7cbece76c9509ee0bb7aec3cca6f4f7cd91
antonepple <antonepple@192.168.178.25>
2012-09-11 13:12:10 +0200

    Changed Folder name

---
+++ Nodes API/ChildFactory asynchronous/Creating children asynchronously/build/classes-generated/de/eppleton/nbexamples/nodes/async/Bundle.java
@@ -0,0 +1,27 @@
+package de.eppleton.nbexamples.nodes.async;
+/** Localizable strings for {@link de.eppleton.nbexamples.nodes.async}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>NodeDisplayer</i>
+     * @see NodeDisplayerTopComponent
+     */
+    static String CTL_NodeDisplayerAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_NodeDisplayerAction");
+    }
+    /**
+     * @return <i>NodeDisplayer Window</i>
+     * @see NodeDisplayerTopComponent
+     */
+    static String CTL_NodeDisplayerTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_NodeDisplayerTopComponent");
+    }
+    /**
+     * @return <i>This is a NodeDisplayer window</i>
+     * @see NodeDisplayerTopComponent
+     */
+    static String HINT_NodeDisplayerTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_NodeDisplayerTopComponent");
+    }
+    private void Bundle() {}
+}
